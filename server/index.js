beca8d170f567ada5f75348a2d3df29b48f38cb4
trybulem <marcin.trybulec@contractors.roche.com>
2016-12-17 13:54:00 +0100

    INIT

---
+++ server/index.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const express = require('express');
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.listen(PORT, ()=> {
+     console.log(`Server Started on port: ${PORT}`);
+});
