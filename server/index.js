d98293efe0fda679d379fc36e1390bf20f7be0e0
trybulem <marcin.trybulec@contractors.roche.com>
2016-12-19 14:00:09 +0100

    2

--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
-"use strict";
+'use strict';
 
 const express = require('express');
+const path = require('path');
+
 const PORT = process.env.PORT || 3000;
+const config = require('./config/environment');
 
 const app = express();
 
+console.log(config.rootDir);
+
+app.use(express.static(path.join('../', __dirname, 'client')));
+
 app.listen(PORT, ()=> {
-     console.log(`Server Started on port: ${PORT}`);
+    console.log(`Server Started on port: ${PORT}`);
 });
