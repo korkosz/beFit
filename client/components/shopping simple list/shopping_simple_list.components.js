77e4a0e5a5e869ce23246630608c482c813e9f29
trybulem <marcin.trybulec@contractors.roche.com>
2017-01-19 16:16:14 +0100

    aaa

---
+++ client/components/shopping simple list/shopping_simple_list.components.js
@@ -0,0 +1,3 @@
+module.exports = {
+    templateUrl: '/components/shopping simple list/shopping_simple_list.template.html'
+};
--- client/index.js
+++ client/index.js
@@ -3,6 +3,7 @@ const angular = require('angular');
 const app = angular.module('beFit', [
     require('angular-ui-router').default,
     require('./common').name,
+    require('./components').name,
     require('./views').name
 ]);
 
