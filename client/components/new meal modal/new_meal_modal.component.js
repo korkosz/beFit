c460c3ea2727217a58d5eedd66142304e6c3877d
trybulem <marcin.trybulec@contractors.roche.com>
2017-01-22 20:02:57 +0100

    eee

--- client/components/new meal modal/new_meal_modal.component.js
+++ client/components/new meal modal/new_meal_modal.component.js
@@ -1,3 +1,37 @@
 module.exports = {
-    templateUrl: '/components/new meal modal/new_meal_modal.template.html'
+    templateUrl: '/components/new meal modal/new_meal_modal.template.html',
+    controller() {
+        let vm = this;
+
+        const tabs = ['FIRST', 'SECOND', 'THIRD'];
+        let activeTabIdx;
+
+        vm.$onInit = function () {
+            activeTabIdx = 0;
+            updateTab();
+        };
+
+        vm.save = function () {
+            vm.$onInit();
+            $('#newMealModal').modal('hide');
+        };
+
+        vm.nextTab = function () {
+            if (activeTabIdx < tabs.length - 1) {
+                activeTabIdx++;
+                updateTab();
+            }
+        };
+
+        vm.prevTab = function () {
+            if (activeTabIdx > 0) {
+                activeTabIdx--;
+                updateTab();
+            }
+        };
+
+        function updateTab() {
+            vm.activeTab = tabs[activeTabIdx];
+        }
+    }
 };
