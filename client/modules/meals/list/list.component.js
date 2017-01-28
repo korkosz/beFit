4e9e9617e1ba6164a4f977b4b6a7ef451cf995e3
trybulem <marcin.trybulec@contractors.roche.com>
2017-01-28 12:03:16 +0100

    zs

---
+++ client/modules/meals/list/list.component.js
@@ -0,0 +1,58 @@
+'use strict';
+
+module.exports = {
+    templateUrl: '/modules/meals/list/list.template.html',
+    controller() {
+        this.showDetails = function () {
+            $('#detailsModal').modal('show');
+        };
+
+        this.closeDetails = function () {
+            $('#detailsModal').modal('hide');
+        }
+
+        this.showNewMealModal = function () {
+            $('#newMealModal').modal('show');
+        };
+
+        this.mealTypes = ['Sniadanie', 'Obiad', 'Kolacja'];
+        this.cookingTimes = ['5min', '10min', '15min', '20min', '25min', '30min'];
+        this.meals = [{
+            name: 'Owsianka',
+            type: 'Sniadanie',
+            time: 15,
+            calories: 650
+        }, {
+            name: 'Owsianka',
+            type: 'Sniadanie',
+            time: 15,
+            calories: 650
+        }, {
+            name: 'Owsianka',
+            type: 'Sniadanie',
+            time: 15,
+            calories: 650
+        }, {
+            name: 'Owsianka',
+            type: 'Sniadanie',
+            time: 15,
+            calories: 650
+        }, {
+            name: 'Owsianka',
+            type: 'Sniadanie',
+            time: 15,
+            calories: 650
+        }, {
+            name: 'Owsianka',
+            type: 'Sniadanie',
+            time: 15,
+            calories: 650
+        }, {
+            name: 'Owsianka',
+            type: 'Sniadanie',
+            time: 15,
+            calories: 650
+        }];
+
+    }
+};
