b1fd8cd75d8df9573967a35f5bf61ab5d72009df
trybulem <marcin.trybulec@contractors.roche.com>
2017-02-13 13:37:40 +0100

    ss

--- client/modules/meals/inline new/inline_new.component.js
+++ client/modules/meals/inline new/inline_new.component.js
@@ -1,3 +1,13 @@
+'use strict';
+
 module.exports = {
-    templateUrl: '/modules/meals/inline new/inline_new.template.html'
+    templateUrl: '/modules/meals/inline new/inline_new.template.html',
+    bindings: {
+        mode: '@',
+        meal: '<'
+    },
+    controller() {
+        var vm = this;
+
+    }
 };
